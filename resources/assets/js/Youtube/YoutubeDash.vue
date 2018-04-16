b4e3caafc8ba3808f5a90bc36268a07888c1eb8b
雷佳玮 <leijiawei@leijiaweideMacBook-Pro.local>
2018-04-16 16:04:01 +0800

    Init Commit 第一次提交项目

---
+++ resources/assets/js/Youtube/YoutubeDash.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="YoutubeDash__wrapper">
+       <video-group :videos="this.videos"></video-group>
+    </div>
+</template>
+
+<script>
+    import VideoGroup from './VideoGroup';
+
+    import Search from './Search';
+
+    export default {
+
+        components: {
+            VideoGroup
+        },
+
+        created() {
+            Search({
+                apiKey: 'AIzaSyAr6HXyB0GO3Vj_rtxy4lSszRxVHROyrbY',
+                term: 'x-men',
+            }, response => this.videos = response);
+        },
+
+        data() {
+          return {
+              videos: null,
+          }
+        }
+    }
+</script>
+
+<style scoped>
+    .YoutubeDash__wrapper {
+
+    }
+</style>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+        <link rel="stylesheet" href="css/app.css">
+        <meta name="csrf-token" content="{{ csrf_token() }}" />
+    </head>
+    <body>
+        <div class="container-fluid" id="app">
+            <div class="row">
+                <div class="col-md">
+                    <youtube-dashboard></youtube-dashboard>
+                </div>
+            </div>
+        </div>
+
+        <script src="js/app.js"></script>
+    </body>
+</html>
